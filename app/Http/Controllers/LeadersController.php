c88a30c706d40c83aae03271859e48df3ccfd03c
mbsica <74180416+mbsica@users.noreply.github.com>
2021-02-16 15:12:04 +0800

    Update LeadersController.php

--- app/Http/Controllers/LeadersController.php
+++ app/Http/Controllers/LeadersController.php
@@ -10,11 +10,10 @@ class LeadersController extends Controller
 {
     //
     public function index(){
-        $stats = DB::table('lhrv_statistics')->get();
         $leaders = DB::table('default_lhrv_alltime')
                    ->join('lhrv_players', 'default_lhrv_alltime.player_id', '=', 'lhrv_players.player_id')
                    ->join('lhrv_statistics', 'default_lhrv_alltime.stat_id', '=', 'lhrv_statistics.stat_id')
-                   ->select('default_lhrv_alltime.*', 'lhrv_players.name', 'lhrv_statistics.real_name')
+                   ->select('default_lhrv_alltime.*', 'lhrv_players.name')
                    ->get();
         $sorted = $leaders->sortByDesc('stat_value');
         return view('profile.leaders', [
@@ -22,7 +21,6 @@ class LeadersController extends Controller
             'playedhours' => $sorted->where('stat_id', '=', 2)->take(100),
             'blockmines' => $sorted->where('stat_id', '=', 4)->take(100),
             'moneys' => $sorted->where('stat_id', '=', 6)->take(100),
-            'stats' => $stats,
         ]);
     }
 }
